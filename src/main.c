Hurricos/realtek-poe

commit 39c93d39dd10da77b4fe48bc1d6bdd3c5978f866

realtek-poe: add ubus dynamic port management
Allow configured ports to be temporarily disabled and re-enabled
without modifying the configuration.

Signed-off-by: Bjørn Mork <bjorn@mork.no>
Tested-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

src/main.c
@@ -908,10 +908,40 @@ ubus_poe_reload_cb(struct ubus_context *ctx, struct ubus_object *obj,
 	return UBUS_STATUS_OK;
 }
 
+static const struct blobmsg_policy ubus_poe_manage_policy[] = {
+	{ "port", BLOBMSG_TYPE_STRING },
+	{ "enable", BLOBMSG_TYPE_BOOL },
+};
+
+static int ubus_poe_manage_cb(struct ubus_context *ctx, struct ubus_object *obj,
+			      struct ubus_request_data *req, const char *method,
+			      struct blob_attr *msg)
+{
+	struct blob_attr *tb[ARRAY_SIZE(ubus_poe_manage_policy)];
+	const struct port_config *port;
+	const char *port_name;
+	size_t i;
+
+	blobmsg_parse(ubus_poe_manage_policy,
+		      ARRAY_SIZE(ubus_poe_manage_policy),
+		      tb, blob_data(msg), blob_len(msg));
+	if (!tb[0] || !tb[1])
+		return UBUS_STATUS_INVALID_ARGUMENT;
+
+	port_name = blobmsg_get_string(tb[0]);
+	for (i = 0; i < config.port_count; i++) {
+		port = &config.ports[i];
+		if (port->enable && !strcmp(port_name, port->name))
+			return poe_cmd_port_enable(i, blobmsg_get_bool(tb[1]));
+	}
+	return UBUS_STATUS_INVALID_ARGUMENT;
+}
+
 static const struct ubus_method ubus_poe_methods[] = {
 	UBUS_METHOD_NOARG("info", ubus_poe_info_cb),
 	UBUS_METHOD_NOARG("reload", ubus_poe_reload_cb),
 	UBUS_METHOD("sendframe", ubus_poe_sendframe_cb, ubus_poe_sendframe_policy),
+	UBUS_METHOD("manage", ubus_poe_manage_cb, ubus_poe_manage_policy),
 };
 
 static struct ubus_object_type ubus_poe_object_type =

commit 6ac3e2b17101def151363168c4390bd14320bfd6

realtek-poe: manage: fix return value on command success
The `ubus call poe manage ...` command returns with an error message:
  "Command failed: Parsing message data failed"

That is obviosly incorrect, and is caused by ubus_poe_manage_cb(),
which returns UBUS_STATUS_INVALID_ARGUMENT instead of UBUS_STATUS_OK
on success.

Change the last return code to UBUS_STATUS_OK.

Fixes: 39c93d39dd10 ("realtek-poe: add ubus dynamic port management")
Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

src/main.c
@@ -905,7 +905,7 @@ static int ubus_poe_manage_cb(struct ubus_context *ctx, struct ubus_object *obj,
 			continue;
 		return poe_cmd_port_enable(mcu, i, blobmsg_get_bool(tb[1]));
 	}
-	return UBUS_STATUS_INVALID_ARGUMENT;
+	return UBUS_STATUS_OK;
 }
 
 static const struct ubus_method ubus_poe_methods[] = {