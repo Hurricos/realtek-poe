Hurricos/realtek-poe

commit e58055c33f12948908111fbde6306a0df7303ecc

tek-poe: allow changing baudrate dialect in the config
A smart program should automatically detect the dialect, including the
baudrate. A program that requires the user to correctly configure
things may be easier to write, but is harder to use.

Ideally, we'd want to send a set of "safe" commands, and based on the
responses, figure out the correct dialect to use. This also applies to
finding out the correct baudrate. I've been meaning to implement this
for a number of years, and I have had neither inspiration, not time to
do so.

Instead of delaying realtek dialect support, add temporary code that
allows the user to override the dialect settings. While not ideal,
this allows realtek-poe to be deployed to, and useful to more users.
It seems better than keeping the realtek dialect support contained to
an obscure branch.

For anyone looking to implement the missing dialect detection, a good
resource for autodetecting serial protocols is gpsd.

Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

src/main.c
@@ -95,19 +95,44 @@ static void load_port_config(struct config *cfg, struct uci_context *uci,
 		cfg->ports[id].power_up_mode = 3;
 }
 
+static void warn_unsupported_config(const char *cfg_name)
+{
+	ULOG_WARN("Option '%s' found in config file.\n"
+		"Support for this option will be removed in the future\n."
+		"You have been warned\n",
+		cfg_name);
+}
+
 static void load_global_config(struct config *cfg, struct uci_context *uci,
 			       struct uci_section *s)
 
 {
-	const char *budget, *guardband;
+	const char *budget, *guardband, *baudrate_hack, *dialect_hack;
 
 	budget = uci_lookup_option_string(uci, s, "budget");
 	guardband = uci_lookup_option_string(uci, s, "guard");
+	baudrate_hack = uci_lookup_option_string(uci, s, "force_baudrate");
+	dialect_hack = uci_lookup_option_string(uci, s, "force_dialect");
 
 	cfg->budget = budget ? strtof(budget, NULL) : 31.0;
 	cfg->budget_guard = cfg->budget / 10;
 	if (guardband)
 		cfg->budget_guard = strtof(guardband, NULL);
+
+	if (baudrate_hack) {
+		warn_unsupported_config("force_baudrate");
+		cfg->forced_baudrate = strtoul(baudrate_hack, NULL, 10);
+	}
+
+	if (dialect_hack) {
+		warn_unsupported_config("force_dialect");
+		if (!strcmp(dialect_hack, "broadcom"))
+			cfg->forced_dialect = &broadcom_dialect;
+		else if (!strcmp(dialect_hack, "realtek"))
+			cfg->forced_dialect = &realtek_dialect;
+		else
+			ULOG_ERR("Unkown dialect '%s'\n", dialect_hack);
+	}
 }
 
 static char *get_board_compatible(void)
@@ -1198,6 +1223,8 @@ static void ubus_connect_handler(struct ubus_context *ctx)
 
 int main(int argc, char **argv)
 {
+	unsigned int baudrate = 0;
+	speed_t dino_baud;
 	int ch;
 
 	struct poe_ctx poe = {
@@ -1215,12 +1242,14 @@ int main(int argc, char **argv)
 	ulog_open(ULOG_STDIO | ULOG_SYSLOG, LOG_DAEMON, "realtek-poe");
 	ulog_threshold(LOG_INFO);
 
-	while ((ch = getopt(argc, argv, "d")) != -1) {
+	while ((ch = getopt(argc, argv, "ds")) != -1) {
 		switch (ch) {
 		case 'd':
 			ulog_threshold(LOG_DEBUG);
 			poe.hardcore_hacking_mode_en = 1;
 			break;
+		case 'f':
+			baudrate = 115200;
 		}
 	}
 
@@ -1229,10 +1258,31 @@ int main(int argc, char **argv)
 	uloop_init();
 	ubus_auto_connect(&poe.conn);
 
+	if (poe.config.forced_dialect)
+		poe.mcu.dialect.desc = poe.config.forced_dialect;
+
 	/* Users of poe_dialect assume the reverse mapping is computed. */
 	dialect_reverse_map(&poe.mcu.dialect);
 
-	if (poe_stream_open("/dev/ttyS1", &poe.mcu.stream, B19200) < 0)
+	/* Prefer '-s' argument over any config file option */
+	if (baudrate) {
+		/* Keep existing baudrate */
+	} else if (poe.config.forced_baudrate) {
+		baudrate = poe.config.forced_baudrate;
+	} else {
+		baudrate = 19200;
+	}
+
+	switch (baudrate) {
+	case 115200:
+		dino_baud = B115200;
+		break;
+	case 19200: /* Fall through */
+	default:
+		dino_baud = B19200;
+		break;
+	}
+	if (poe_stream_open("/dev/ttyS1", &poe.mcu.stream, dino_baud) < 0)
 		return -1;
 
 

src/tek-poe.h
@@ -116,10 +116,15 @@ struct port_config {
 	uint8_t power_budget;
 };
 
+struct dialect_desc;
+
 struct config {
+	const struct dialect_desc *forced_dialect;
+
 	float budget;
 	float budget_guard;
 
+	unsigned int forced_baudrate;
 	unsigned int port_count;
 	uint8_t pse_id_set_budget_mask;
 	struct port_config ports[MAX_PORT];

commit a79d1bbb427dbbf59ac1ed92a8bd5fbd223b6d6f

realtek-poe: remove commandline argument for baudrate
Commit e58055c ("tek-poe: allow changing baudrate dialect in the
config") introduced two ways to override the default baudrate. Having
a commandline parameter to change the baudrate was useful during
development, but should not have been committed otherwise. This
function does not work due to a typo in the getopt() arguments.

The design philosophy of realtek-poe is that it should work with
minimal configuration. Because an auto-detection mechanism is not
currently implemented, the baudrate needs to be configured by the
user. However doing so from two inputs could be confusing.

Remove the commandline argument to prevent confusion. The
`force_baudrate` config option be used instead.

Fixes: e58055c ("tek-poe: allow changing baudrate dialect in the config")
Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

src/main.c
@@ -1276,14 +1276,12 @@ int main(int argc, char **argv)
 	ulog_open(ULOG_STDIO | ULOG_SYSLOG, LOG_DAEMON, "realtek-poe");
 	ulog_threshold(LOG_INFO);
 
-	while ((ch = getopt(argc, argv, "ds")) != -1) {
+	while ((ch = getopt(argc, argv, "d")) != -1) {
 		switch (ch) {
 		case 'd':
 			ulog_threshold(LOG_DEBUG);
 			poe.hardcore_hacking_mode_en = 1;
 			break;
-		case 'f':
-			baudrate = 115200;
 		}
 	}
 
@@ -1298,10 +1296,8 @@ int main(int argc, char **argv)
 	/* Users of poe_dialect assume the reverse mapping is computed. */
 	dialect_reverse_map(&poe.mcu.dialect);
 
-	/* Prefer '-s' argument over any config file option */
-	if (baudrate) {
-		/* Keep existing baudrate */
-	} else if (poe.config.forced_baudrate) {
+	/* Temporarily allow config file to override baudrate */
+	if (poe.config.forced_baudrate) {
 		baudrate = poe.config.forced_baudrate;
 	} else {
 		baudrate = 19200;